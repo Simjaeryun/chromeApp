761e701b56ba24165eb9418279c85173df009de7
Simjaeryun <61261788+Simjaeryun@users.noreply.github.com>
2020-09-29 15:13:40 +0900

    initial js

---
+++ js/weather.js
@@ -0,0 +1,57 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "bfd963a4ab3bab3b8e923c431315e3ca";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${Math.floor(temperature - 273.15)}도 @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("위치를 찾을수 없습니다.");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
